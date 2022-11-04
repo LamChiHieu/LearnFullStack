b2e530c926d6f19bad95825d4cd01d2356052593
Lam Chi Hieu <lamchihieu.vnn@gmail.com>
2022-11-04 11:02:23 +0700

    1

--- NodeJs/src/server.js
+++ NodeJs/src/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import bodyParser from "body-parser";
 let viewEngine = require("./config/viewEngine");
 let innitWebRoutes = require("./route/web");
+let connectDB = require("./config/connectDB");
 var env = require('dotenv');
 
 let app = express();
@@ -11,6 +12,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 viewEngine(app);
 innitWebRoutes(app);
+connectDB();
 
 env.config();
 let port = process.env.PORT || 6969;
